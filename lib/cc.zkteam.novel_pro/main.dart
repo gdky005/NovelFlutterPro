064f47770c9aaa94de3a61096395613c544b341f
WangQing <wangqing11@100tal.com>
2019-09-05 18:45:57 +0800

    1. 添加首页。

--- lib/cc.zkteam.novel_pro/main.dart
+++ lib/cc.zkteam.novel_pro/main.dart
@@ -77,81 +77,101 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("短小说"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '计数器：',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Text("第一本短小说名字："),
-            Text(text, style: Theme.of(context).textTheme.body1),
-          ],
-        ),
-      ),
-      drawer: Drawer(
-          child: Column(
-        children: <Widget>[
-          DrawerHeader(
-            padding: EdgeInsets.all(0),
-            child: Theme(
-                data: Theme.of(context)
-                    .copyWith(primaryColor: Colors.transparent),
-                child: UserAccountsDrawerHeader(
-                  currentAccountPicture: CircleAvatar(
-                      child: Image.network(
-                          "https://www.gdky005.com/images/avatar.jpg")),
-                  accountName: Text('孤独狂饮'),
-                  accountEmail: Text('741227905@qq.com'),
-                )),
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567688759859&di=f00ae6e9b10ee3d5970b5f00567fce4f&imgtype=0&src=http%3A%2F%2Fimg.ph.126.net%2FlCFW6F0A46eced8IjrhWOQ%3D%3D%2F2570148012361858430.jpg"))),
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("短小说"),
+            bottom: TabBar(
+                unselectedLabelColor: Colors.white54,
+                indicatorColor: Colors.white,
+                indicatorSize: TabBarIndicatorSize.label,
+                tabs: [
+                  Tab(icon: Icon(Icons.live_tv)),
+                  Tab(icon: Icon(Icons.local_airport)),
+                  Tab(icon: Icon(Icons.local_cafe)),
+                ]),
           ),
-          ListTile(
-            title: Text('测试 Item'),
-            leading: Icon(Icons.title),
-            onTap: () {
-              Navigator.of(context).pop();
-              ZKToast.show(context, "测试的 Item");
-            },
-          ),
-          ListTile(
-              title: Text('... 添加更多条 ...'),
-              leading: Icon(Icons.more_horiz),
-              onTap: () {
-                Navigator.of(context).pop();
-                ZKToast.show(context, "请添加更多 Item 吧");
-              }),
-          AboutListTile(
-              icon: Icon(Icons.nature_people),
-              applicationName: "短小说",
-              applicationVersion: "1.0.0",
-              applicationIcon: CircleAvatar(child: Icon(Icons.near_me)),
-              applicationLegalese: "协议",
-              aboutBoxChildren: [
-                ListTile(
-                  title: Text('成员'),
-                  leading: Icon(Icons.people),
-                ),
-              ]),
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          body: TabBarView(children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.local_cafe,
+                      size: 128, color: Colors.lightBlueAccent[700]),
+                  SizedBox(height: 100),
+                  Text(
+                    '计数器：',
+                  ),
+                  Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.display1,
+                  ),
+                  Text("第一本短小说名字："),
+                  Text(text, style: Theme.of(context).textTheme.body1),
+                ],
+              ),
+            ),
+            Icon(Icons.local_airport,
+                size: 128, color: Colors.lightBlueAccent[700]),
+            Icon(Icons.local_cafe,
+                size: 128, color: Colors.lightBlueAccent[700]),
+          ]),
+          drawer: Drawer(
+              child: Column(
+            children: <Widget>[
+              DrawerHeader(
+                padding: EdgeInsets.all(0),
+                child: Theme(
+                    data: Theme.of(context)
+                        .copyWith(primaryColor: Colors.transparent),
+                    child: UserAccountsDrawerHeader(
+                      currentAccountPicture: CircleAvatar(
+                          child: Image.network(
+                              "https://www.gdky005.com/images/avatar.jpg")),
+                      accountName: Text('孤独狂饮'),
+                      accountEmail: Text('741227905@qq.com'),
+                    )),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567688759859&di=f00ae6e9b10ee3d5970b5f00567fce4f&imgtype=0&src=http%3A%2F%2Fimg.ph.126.net%2FlCFW6F0A46eced8IjrhWOQ%3D%3D%2F2570148012361858430.jpg"))),
+              ),
+              ListTile(
+                title: Text('测试 Item'),
+                leading: Icon(Icons.title),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  ZKToast.show(context, "测试的 Item");
+                },
+              ),
+              ListTile(
+                  title: Text('... 添加更多条 ...'),
+                  leading: Icon(Icons.more_horiz),
+                  onTap: () {
+                    Navigator.of(context).pop();
+                    ZKToast.show(context, "请添加更多 Item 吧");
+                  }),
+              AboutListTile(
+                  icon: Icon(Icons.nature_people),
+                  applicationName: "短小说",
+                  applicationVersion: "1.0.0",
+                  applicationIcon: CircleAvatar(child: Icon(Icons.near_me)),
+                  applicationLegalese: "协议",
+                  aboutBoxChildren: [
+                    ListTile(
+                      title: Text('成员'),
+                      leading: Icon(Icons.people),
+                    ),
+                  ]),
+            ],
+          )),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ));
   }
 }
