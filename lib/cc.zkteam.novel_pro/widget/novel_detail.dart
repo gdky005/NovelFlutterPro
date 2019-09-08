21c7e745103f05c91050dbf51c1c7b8b88686eb6
653049738@qq.com <653049738@qq.com>
2019-09-08 10:04:39 +0800

    小说详情页

---
+++ lib/cc.zkteam.novel_pro/widget/novel_detail.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+
+
+class NovelDetailPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios,
+                size: 18, color: Theme.of(context).primaryColor),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: Text(
+            '详情',
+            style: TextStyle(
+                fontWeight: FontWeight.w300, fontSize: 18.0),
+          ),
+          elevation: 0.0,
+        ),
+        body: _novelDetail(context));
+  }
+
+  Widget _novelDetail(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.only(
+            top: 10.0, left: 20.0, right: 20.0, bottom: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _titleWidget(),
+            _timeWidget(),
+            _detailsWeb(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _titleWidget() {
+    return Container(
+      child: Text(
+        '标题',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+
+  Widget _timeWidget() {
+    return Container(
+      padding: EdgeInsets.only(top: 20.0, bottom: 30.0),
+      child: Text(
+        '时间',
+        style: TextStyle(
+          fontSize: 12.0,
+          color: Colors.grey,
+        ),
+      ),
+    );
+  }
+
+  Widget _detailsWeb() {
+    return Container(
+      child: Html(data: '详情内容详情内容详情内容详情内容'),
+    );
+  }
+}
