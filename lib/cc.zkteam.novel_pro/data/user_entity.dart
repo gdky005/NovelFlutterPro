f15ecdf68477680fc9090f2029ad5031555514a2
WangQing <wangqing11@100tal.com>
2019-09-15 14:10:00 +0800

    1. 修复 json 转换类出现的异常。

--- lib/cc.zkteam.novel_pro/data/user_entity.dart
+++ lib/cc.zkteam.novel_pro/data/user_entity.dart
@@ -1,7 +1,12 @@
-class UserEntity{
-  UserEntity({this.nickName,this.email,this.avatar});
+class UserEntity {
+  UserEntity({this.nickName, this.email, this.avatar});
   String nickName;
   String email;
   String avatar;
 
+  UserEntity.fromJson(Map<String, dynamic> json) {
+    nickName = json['nickName'];
+    email = json['email'];
+    avatar = json['avatar'];
+  }
 }
